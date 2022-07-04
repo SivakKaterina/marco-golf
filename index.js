a38856089a38d3a74aa3c43673aa3c091b11d78a
SivakKaterina <72225534+SivakKaterina@users.noreply.github.com>
2022-07-04 12:11:04 +0300

    add map

---
+++ index.js
@@ -0,0 +1,14 @@
+function initMap() {
+  const pos = { lat: 25.9210802, lng: -81.7250113 };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 15,
+    center: pos,
+  });
+  const marker = new google.maps.Marker({
+    position: pos,
+    map: map,
+    title: "Hello World!",
+  });
+}
+
+window.initMap = initMap;
